edd431d6c6b0bdcc70ff4db646cae32ce77372d2
CristiPaul <32931823+CristiPaul@users.noreply.github.com>
2020-05-15 07:55:09 +0300

    Add files via upload

---
+++ Context/BibliotecaContext.cs
@@ -0,0 +1,24 @@
+﻿using Biblioteca.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Context
+{
+    public class BibliotecaContext: IdentityDbContext <IdentityUser>
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+    }
+}
